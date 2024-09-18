762e85cdc01d80c73c0882bcc4dc35ce87cf8ff1
Yanwen Xu <xuyanwen2012@gmail.com>
2024-09-17 23:30:37 -0700

    save

--- scripts/create-vm copy.fish
+++ scripts/create-vm copy.fish
@@ -1,8 +1,51 @@
 #!/usr/bin/env fish
 
 set -g STORAGE local-zfs
-set -g USER doremy
 set -g VENDOR_LOCATIONS /var/lib/vz/snippets
+set -g USER doremy
+
+# list of image urls (latest versions)
+set -g IMAGE_URLS "debian=https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2" \
+    "ubuntu=https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img" \
+    "fedora=https://download.fedoraproject.org/pub/fedora/linux/releases/40/Cloud/x86_64/images/Fedora-Cloud-Base-Generic.x86_64-40-1.14.qcow2"
+
+# [helper] the download file name is the last part of the url after the last '/'
+function get_download_name
+    set url $argv[1]
+    set file_name (string split -m 1 -r / $url)
+    echo $file_name[-1]
+end
+
+# this function takes in the image name (key) and downloads the image
+function get_image
+    set os_name $argv[1]
+    set image_url (echo $IMAGE_URLS | grep -oP "$os_name=\K[^ ]+")
+
+    set file_name (get_download_name $image_url)
+
+    # check if image link exists
+    if test -z $file_name
+        echo "Image not found"
+        return 1
+    end
+
+    # check if file already exists, prompt user to overwrite
+    if test -f $file_name
+        echo "File $file_name already exists. Overwrite? [y/n]"
+        read -l overwrite
+        if test $overwrite = n
+            return 0
+        end
+    end
+
+    echo "Downloading $file_name from $image_url"
+
+    # download the image and resize it to 32G
+    wget --show-progress -O $file_name $image_url
+
+    qemu-img resize $file_name 32G
+end
+
 
 # Function to create the VM
 function create_vm
@@ -64,7 +107,15 @@ function create_cloudinit_config
 end
 
 
+# read id and name from command line
+if test (count $argv) -lt 3
+    echo "Usage: create_vm <vmid> <vm_name> <cloud-init-yaml>"
+else
+    create_vm $argv[1] $argv[2]
+    import_disk $argv[1] $argv[3]
+    create_cloudinit_config $argv[1] $argv[3]
+end
 
-create_vm 666 test-dabian
-import_disk 666 debian-12-generic-amd64.qcow2
-create_cloudinit_config 666 debian
+# create_vm 666 test-dabian
+# import_disk 666 debian-12-generic-amd64.qcow2
+# create_cloudinit_config 666 debian
